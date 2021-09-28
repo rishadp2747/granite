298d292fccea77e86a29669a9029a5e49b7810ec
rishadp <rishadp2747@gmail.com>
2021-09-28 19:10:53 +0530

    Added login feature

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   defaults format: :json do
     resources :tasks, except: %i[new edit], param: :slug
     resources :users, only: %i[index create]
+    resource :sessions, only: :create
   end
 
   root "home#index"
