90b36630de315b8212eda3a912fcef8c2263ce3a
rishadp <rishadp2747@gmail.com>
2021-09-29 16:55:30 +0530

    Fixed order of items in ActiveRecord models

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class User < ApplicationRecord
-  VALID_EMAIL_REGEX = /\A([\w+\-].?)+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i.freeze
+  VALID_EMAIL_REGEX = /\A([\w+\-].?)+@[a-z\d\-]+(\. :a-z]+)*\.[a-z]+\z/i.freeze
 
   has_many :comments, dependent: :destroy
   has_many :tasks, dependent: :destroy, foreign_key: :user_id
@@ -10,11 +10,15 @@ class User < ApplicationRecord
   has_secure_token :authentication_token
 
   validates :name, presence: true, length: { maximum: 35 }
-  validates :email, presence: true,
-                    uniqueness: true,
-                    length: { maximum: 50 },
-                    format: { with: VALID_EMAIL_REGEX }
-  validates :password, length: { minimum: 6 }, if: -> { password.present? }
+  validates :email,
+    presence: true,
+    uniqueness: true,
+    length: { maximum: 50 },
+    format: { with: VALID_EMAIL_REGEX }
+  validates :password,
+    presence: true,
+    confirmation: true,
+    length: { minimum: 6 }
   validates :password_confirmation, presence: true, on: :create
 
   before_save :to_lowercase
